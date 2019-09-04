2176341b11b9e6aad0e5a95e60aeac375eed38ea
Danielius Visockas <danieliusvisockas@gmail.com>
2019-09-04 22:40:25 +0300

    Implement step function

--- 1_pirmas/linear.py
+++ 1_pirmas/linear.py
@@ -5,17 +5,19 @@ data = Data('data.txt').load()
 
 w1, w2, b = [(random.randint(0,100) / 100) for i in list(range(0, 3))]
 
-# Learning rate could be found in a similar fashion to weights and bias
-lr = 0.001
+lr = 0.01
+num_epochs = 1000
+epochs = range(1, num_epochs)
 
 def output(x1, x2):
-  return x1 * w1 + x2 * w2 + b
+  o = x1 * w1 + x2 * w2 + b
+  return 1 if o > 0 else -1
 
-for i in list(range(0, 10)):
+for epoch in epochs:
   for row in data:
     x1, x2, y = row
     y_hat = output(x1, x2)
-    error = y_hat - y
+    error = y - y_hat
     w1 = w1 + (lr * error * x1)
     w2 = w2 + (lr * error * x2)
     b = b + (lr * error)
@@ -23,4 +25,4 @@ for i in list(range(0, 10)):
 for row in data:
   x1, x2, y = row
   predicted = output(x1, x2)
-  print(f'Real: {y}, predicted: {round(predicted, 4)}')
+  print(f'Real: {int(y)}, predicted: {round(predicted, 100)}')
