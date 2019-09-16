543f17307844889a0891478d8f9ccf170b762d14
Danielius Visockas <danieliusvisockas@gmail.com>
2019-09-16 15:23:12 +0300

    Use train/val splits

--- 1_pirmas/data.py
+++ 1_pirmas/data.py
@@ -1,9 +1,32 @@
+from collections import defaultdict
+
 class Data(object):
-  def __init__(self, filename):
+  def __init__(self, filename = 'data.txt'):
     self.filename = filename
+    self.load()
 
   def load(self):
     file_data = open(self.filename).read().split('\n')
-    return [[float(item) for item in row.split(',')] for row in file_data]
+    self.data = [[float(item) for item in row.split(',')] for row in file_data]
 
+  def get_train_valid(self):
+    percentage_train = 0.8
+    return self.data_by_classes()
 
+  def data_by_classes(self):
+    percent_train = 0.8
+
+    data = defaultdict(lambda: [])
+    for datum in self.data:
+      klass = str(int(datum[-1]))
+      data[klass].append(datum)
+
+    split_data = [[], []]
+
+    for key in list(data.keys()):
+      class_items = data[key]
+      n_train = int(len(class_items) * percent_train)
+      [split_data[0].append(item) for item in class_items[:n_train]]
+      [split_data[1].append(item) for item in class_items[n_train:]]
+
+    return split_data
--- 1_pirmas/linear.py
+++ 1_pirmas/linear.py
@@ -1,7 +1,7 @@
 import random
 from data import Data
 
-data = Data('data.txt').load()
+train_data, validation_data = Data().get_train_valid()
 
 w1, w2, b = [(random.randint(0,100) / 100) for i in list(range(0, 3))]
 
@@ -14,7 +14,7 @@ def output(x1, x2):
   return 1 if o > 0 else -1
 
 for epoch in epochs:
-  for row in data:
+  for row in train_data:
     x1, x2, y = row
     y_hat = output(x1, x2)
     error = y - y_hat
@@ -22,7 +22,7 @@ for epoch in epochs:
     w2 = w2 + (lr * error * x2)
     b = b + (lr * error)
 
-for row in data:
+for row in validation_data:
   x1, x2, y = row
   predicted = output(x1, x2)
   print(f'Real: {int(y)}, predicted: {round(predicted, 100)}')
