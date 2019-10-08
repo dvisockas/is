11e7b33dab94629cba8da139e2f1aec27e51bbf8
Danielius Visockas <danieliusvisockas@gmail.com>
2019-10-08 10:46:55 +0300

    Activation functions

--- 2_antras/net.py
+++ 2_antras/net.py
@@ -1,35 +1,37 @@
 from neuron import Neuron
 
 class Net(object):
-  def __init__(self, inputs, outputs, hidden_dimensions, activation = 'sigmoid'):
+  def __init__(self, inputs, outputs, hidden_dimensions, activation = 'tanh'):
     self.input_size = len(inputs[0])
     self.output_size = len(outputs[0])
+    self.hidden_dimensions = hidden_dimensions
+    self.create_neurons()
+
+  def create_neurons(self):
+    dims = self.hidden_dimensions
     self.layers = []
 
+    for idx, layer in enumerate(dims):
+      in_count = self.input_size if idx == 0 else dims[idx - 1]
 
-    for idx, layer in enumerate(hidden_dimensions):
-      in_count = self.input_size if idx == 0 else hidden_dimensions[idx - 1]
-
-      layer_neurons = [Neuron(in_count) for layer in hidden_dimensions]
+      layer_neurons = [Neuron(in_count) for layer in dims]
 
       self.layers.append(layer_neurons)
 
-    self.layers.append([Neuron(hidden_dimensions[-1], activation = 'softmax')])
+    self.layers.append([Neuron(dims[-1], activation = 'softmax')])
 
   def forward(self, input_vector):
     outs = input_vector
 
     for layer in self.layers:
       tmp_outs = []
+
       for neuron in layer:
-        weighted_values = [neuron.weights[idx] * x_in for idx, x_in in enumerate(outs)]
-        tmp_outs.append(sum(weighted_values) + neuron.bias)
+        tmp_outs.append(neuron.forward(outs))
 
       outs = tmp_outs
 
     return outs
-
-
 
   def backward(parameter_list):
     pass
--- 2_antras/neuron.py
+++ 2_antras/neuron.py
@@ -1,4 +1,27 @@
+from math import exp
+
 class Neuron(object):
   def __init__(self, inputs, activation = 'sigmoid'):
+    self.activation = activation
     self.weights = list(range(0, inputs))
     self.bias = 0.1
+
+  def forward(self, ins):
+    weighted_values = [self.weights[idx] * x_in for idx, x_in in enumerate(ins)]
+    weight_bias = sum(weighted_values) + self.bias
+    return self.activate(weight_bias)
+
+  def activate(self, weight_bias):
+    return activations[self.activation](weight_bias)
+
+  def activations(weight_bias):
+    return {
+      'tanh': tanh,
+      'sigmoid': tanh,
+      'softmax': tanh,
+    }
+
+  def tanh(x):
+    return (2 / ( 1 + exp( -1 * x ) ))-1
+
+
